34209523b52490da60a470e3248c7b37ccdef2ea
Art Ruiz <a.ruiz.jr@gmail.com>
2021-07-18 23:29:55 -0500

    updated class variable to be state pattern to observe update behavior. added "selected" prop onto list item if ancestry type is chosen.

--- react-app/src/App/Ancestry/index.tsx
+++ react-app/src/App/Ancestry/index.tsx
@@ -4,26 +4,32 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Grid from '@material-ui/core/Grid';
-import { textChangeRangeIsUnchanged } from 'typescript';
 
-type Props = {};
+type State = {
+  chosenCd: string;
+  oAbilities: Abilities;
+};
 
 class Ancestry extends React.Component {
-  chosenCd: string;
-  oAbilities : Abilities;
+  state: State = {
+    chosenCd: 'Dwarf',
+    oAbilities: new Abilities(),
+  };
+  ancestryTypes: string[] = ['Dwarf', 'Elf', 'Gnome', 'Halfling', 'Human', 'Orc'];
 
-  constructor(props: Props) {
-    super(props);
-    this.chosenCd = 'Dwarf';
-    this.oAbilities = new Abilities();
+  is_selected_item(ancestryType: string): boolean {
+    return ancestryType === this.state.chosenCd;
   }
 
   set_chosenCd(value: string) {
-    this.chosenCd = value;
+    this.setState({
+      ...this.state,
+      chosenCd: value,
+    });
   }
 
   get_chosenCd() {
-    return this.chosenCd;
+    return this.state.chosenCd;
   }
 
   renderRow(text: string) {
@@ -31,92 +37,77 @@ class Ancestry extends React.Component {
       <ListItem button key={text}>
         <ListItemText primary={text} />
       </ListItem>
-    )
-  }
-
-  get_List() {
-    return (
-      <div>
-        <form className="ui form">
-          <Grid container spacing={5} xs={6} alignItems="center">
-            <Grid item xs={6}>
-              <List>
-                {['Dwarf', 'Elf', 'Gnome', 'Halfling', 'Human', 'Orc'].map((text, index) => (
-                <ListItem onClick={(e) => this.onSelect(text)} button key={text}>
-                  <ListItemText primary={text} />
-                </ListItem>
-            // eslint-disable-next-line @typescript-eslint/comma-dangle
-                ))}
-              </List>
-           </Grid>
-          <Grid container xs={6}>
-            {this.get_Abilities()}
-          </Grid>
-        </Grid>
-      </form>
-    </div>
     );
   }
 
   get_Abilities() {
     return (
-      <Grid container spacing={5} xs={9}>
+      <Grid container spacing={5}>
         <Grid item xs={6}>
-          Strength: 
-          </Grid>
-        <Grid item xs={3}>
-          {this.oAbilities.get_str()}
-        </Grid>          
-      <Grid item xs={6}>
-        <div>Dexterity: </div>
+          Strength:
         </Grid>
         <Grid item xs={3}>
-        <div>{this.oAbilities.get_dex()}</div>
-        </Grid>        
-      <Grid item xs={6}>
-        <div>Constitution: </div>
+          {this.state.oAbilities.get_str()}
+        </Grid>
+        <Grid item xs={6}>
+          <div>Dexterity: </div>
         </Grid>
         <Grid item xs={3}>
-        <div>{this.oAbilities.get_con()}</div>
+          <div>{this.state.oAbilities.get_dex()}</div>
         </Grid>
-      <Grid item xs={6}>
-        <div>Intelligence: </div>
-        </Grid>
-        <Grid item xs={3}>
-        <div>{this.oAbilities.get_int()}</div>
-        </Grid>
-      <Grid item xs={6}>
-        <div className="column">Wisdom: </div>
+        <Grid item xs={6}>
+          <div>Constitution: </div>
         </Grid>
         <Grid item xs={3}>
-        <div className="column">{this.oAbilities.get_wis()}</div>
+          <div>{this.state.oAbilities.get_con()}</div>
         </Grid>
-      <Grid item xs={6}>
-        <div className="column">Charisma: </div>
+        <Grid item xs={6}>
+          <div>Intelligence: </div>
         </Grid>
         <Grid item xs={3}>
-        <div className="column">{this.oAbilities.get_cha()}</div>
-        </Grid>             
+          <div>{this.state.oAbilities.get_int()}</div>
+        </Grid>
+        <Grid item xs={6}>
+          <div className="column">Wisdom: </div>
+        </Grid>
+        <Grid item xs={3}>
+          <div className="column">{this.state.oAbilities.get_wis()}</div>
+        </Grid>
+        <Grid item xs={6}>
+          <div className="column">Charisma: </div>
+        </Grid>
+        <Grid item xs={3}>
+          <div className="column">{this.state.oAbilities.get_cha()}</div>
+        </Grid>
       </Grid>
     );
   }
 
   onSelect(text: string) {
     this.set_chosenCd(text);
-    console.log(this.get_chosenCd());
   }
 
   render() {
     return (
-      // <List>
-      //   {['Dwarf', 'Elf', 'Gnome', 'Halfling', 'Human', 'Orc'].map((text, index) => (
-      //     <ListItem button key={text}>
-      //       <ListItemText primary={text} />
-      //     </ListItem>
-      //     // eslint-disable-next-line @typescript-eslint/comma-dangle
-      //   ))}
-      // </List>
-      this.get_List()
+      <Grid container spacing={5} alignItems="center">
+        <Grid item xs={6}>
+          <List>
+            {this.ancestryTypes.map((text) => (
+              <ListItem
+                selected={this.is_selected_item(text)}
+                onClick={() => this.onSelect(text)}
+                button
+                key={text}
+              >
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
+        <Grid item xs={6}>
+          {this.get_Abilities()}
+        </Grid>
+      </Grid>
     );
   }
 }
